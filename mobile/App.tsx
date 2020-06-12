6fd775e97733807bcdb2b3142bf688677f852da0
André da Silva Barbosa <mk.meiokilous@gmail.com>
2020-06-12 07:51:58 -0300

    Versão Inicial Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, StatusBar, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent></StatusBar>
+      <Routes/>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
